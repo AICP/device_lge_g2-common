c7d9537a7f8fb6d475ea8a254eb06179e3df43dc
dr87 <nb00870@gmail.com>
2014-05-24 14:38:00 -0400

    Copy sec_config to system/etc
    File was present but wasn't being copied.
    
    Change-Id: I9aa1b5e0fccf2e5f82b5bf504947e55da6f12ad7
    Signed-off-by: dr87 <nb00870@gmail.com>

--- g2.mk
+++ g2.mk
@@ -51,7 +51,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sec_config:system/etc/sec_config
 
 # Wifi
 PRODUCT_COPY_FILES += \
