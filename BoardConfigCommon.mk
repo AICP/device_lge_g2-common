ed04edad8d8404e0e603b1cf3b076ccc8a9e3bfd
Rashed Abdel-Tawab <rashed@linux.com>
2014-11-21 18:26:17 -0500

    Revert "g2: Switch to NEON and VFP v4"
    This reverts commit 10ab41353780f139c90eb5d921020bdb64a8cbad.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,8 +23,8 @@ TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 
 # CPU
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
