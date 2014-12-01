9a07b914cb79a383ebbffe42d667e9ef371af4e4
Rashed Abdel-Tawab <rashed@linux.com>
2014-12-01 16:14:30 -0500

    g2: Add TARGET_KERNEL_ARCH := arm
    Change-Id: I575ef9556007f7fb9b80bbed2a6781f7d562fe74

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2-common/releasetools/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/lge/msm8974
+TARGET_KERNEL_ARCH := arm
 
 BOARD_USES_ALSA_AUDIO:= true
 
