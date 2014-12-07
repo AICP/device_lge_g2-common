ececdae9092ffb22cb63eee668f0f61678ee3991
Drgravy <drg113001@gmail.com>
2014-12-06 20:48:48 -0600

    g2: enable multi-color led change wifi scan to 600
    Change-Id: Ie5009e49301a81832fec27ff3f79ec50d1b588a2

--- g2.mk
+++ g2.mk
@@ -205,7 +205,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+	wifi.supplicant_scan_interval=600
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
